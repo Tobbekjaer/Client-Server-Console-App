aa540eea88033dc88cfac434645ce40f7cfb5940
Tobias Kjær <tobias.hougs.kjaer@gmail.com>
2024-03-07 21:18:32 +0100

    Com

--- ServerConsole/obj/Debug/net8.0/ServerConsole.AssemblyInfo.cs
+++ ServerConsole/obj/Debug/net8.0/ServerConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b314fb30f270306b7b9600ff77418ad7ee1bd9b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27cdcf9db8303e06a9c79e8bf2bf0138bd130a3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
