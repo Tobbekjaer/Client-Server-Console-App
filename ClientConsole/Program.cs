ebf5703c8ad2c0b2bcfd257675cce84025669b08
Tobias Kjær <tobias.hougs.kjaer@gmail.com>
2024-03-07 14:07:57 +0100

    Client-Server Console Application

---
+++ ClientConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+
+
+// A C# program for Client
+namespace ClientConsole
+{
+    public class Program
+    {
+        // Main Method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        // ExecuteClient() Method
+        static void ExecuteClient()
+        {
+            try
+            {
+                // #1: We establish the remote endpoint for the socket. 
+                // This example uses port 11111 on the local computer.
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress iPAddress = ipHost.AddressList[0];
+                IPEndPoint localEndPoint  = new IPEndPoint(iPAddress, 11111); 
+                
+                // #2: We create a TCP/IP Socket using the Socket Class Constructor
+
+            }
+
+        }
+    }
+}
